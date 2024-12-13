8af97142ab5a12ddb8c7dc25e10e6ac086929a7c
vladimir <wladimir.burlay@gmail.com>
2024-12-13 11:44:46 +0100

    main.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import mlflow
 from steps.image_data_generator import *
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/")
+async def root():
+    return {"detail_check": "OK", "model_version": 1}
+@app.get("/predict")
+async def predict():
+    logged_model = 'runs:/9be3261b65cf49fdb1812a0c0a3828c8/resnet50'
+    loaded_model = mlflow.pyfunc.load_model(logged_model)
+    generator_train, generator_validation = train_val_generators()
+    image_batch, y_true = next(iter(generator_validation))
+    y_pred = loaded_model.predict(image_batch)
+    predict = np.array([1 if x >= 0.5 else 0 for x in y_pred])
 
-logged_model = 'runs:/9be3261b65cf49fdb1812a0c0a3828c8/resnet50'
+    return {"predicted_class": predict.tolist()}
 
-# Load model as a PyFuncModel.
-loaded_model = mlflow.pyfunc.load_model(logged_model)
-generator_train, generator_validation = train_val_generators()
-image_batch, y_true = next(iter(generator_validation))
-y_pred = loaded_model.predict(image_batch)
-predict = np.array([1 if x >= 0.5 else 0 for x in y_pred])
-print(predict)
